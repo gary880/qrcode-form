c31857229a7becb1137913d27e56703af2c38436
Dylan Chen <dylanchen@sinbon.com>
2024-07-18 15:25:32 +0800

    add basename on broswerRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import UserInfo from './pages/UserInfo';
 import QRScanner from './pages/QRScanner';
 import Signature from './pages/Signature';
@@ -14,14 +14,14 @@ function App() {
   });
 
   return (
-    <Router>
+    <BrowserRouter basename="/qrcode-form">
       <Routes>
         <Route path="/" element={<UserInfo userData={userData} setUserData={setUserData} />} />
         <Route path="/qrscanner" element={<QRScanner userData={userData} setUserData={setUserData} />} />
         <Route path="/signature" element={<Signature userData={userData} setUserData={setUserData} />} />
         <Route path="/summary" element={<Summary userData={userData} setUserData={setUserData} />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
